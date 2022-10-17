dbca7585f559999c7e4abce6b023f7197edc8778
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-10-17 13:36:22 -0700

    Tested each statement
    to see which statement produced the total calculated value (C)

---
+++ myQuiz.py
@@ -0,0 +1,15 @@
+#Statement A
+#import Budget
+#NO
+
+#Statement B
+#import Budget;print(calcBills())
+#NO
+
+#Statement C
+import Budget;print(Budget.calcBills())
+#YES
+
+#Statement D
+#print(Budget.calcBills())
+#NO
