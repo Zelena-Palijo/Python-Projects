4f7ab0d452e48d60ff75b171bc660956f69af87b
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-05 15:28:34 -0700

    Added button and entry for source, destination

--- file_transfer.py
+++ file_transfer.py
@@ -8,6 +8,27 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self)
         self.master.title("File Transfer") #title of GUI window
+        #Creates button to select files frm source directory
+        self.sourceDir_btn = Button(text="Select Source", width=20)
+        #Positions source button in GUI using tkinter grid()
+        self.sourceDir_btn.grid(row=0, column=0, padx=(20,10), pady=(30,0))
+
+        #Creates entry for source directory selection
+        self.source_dir = Entry(width=75)
+        #Positions entry in the GUI
+        #padx and pady are the same as button so they line up
+        self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20,10), pady=(30,0))
+
+        #Creates button to select destination of files from destination directory
+        self.destDir_btn = Button(text="Select Destination", width=20)
+        #Positions destination button in GUI using tkinter grid()
+        #on the next row under the source button
+        self.destDir_btn.grid(row=1, column=0, padx=(20,10), pady=(15,10))
+
+        #Creates entry for destination directory selection
+        self.destination_dir = Entry(width=75)
+        #Positions entry in GUI, line up buttons
+        self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20,10), pady=(15,10))
 
 
 if __name__ == "__main__":
