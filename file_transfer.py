55eda1657fda8206d29bfc9b76b1ff219a9060a2
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-05 15:39:35 -0700

    added destination entry

--- file_transfer.py
+++ file_transfer.py
@@ -20,7 +20,7 @@ class ParentWindow(Frame):
         self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20,10), pady=(30,0))
 
         #Creates button to select destination of files from destination directory
-        self.destDir_btn = Button(text="Select Destination", width=20)
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
         #Positions destination button in GUI using tkinter grid()
         #on the next row under the source button
         self.destDir_btn.grid(row=1, column=0, padx=(20,10), pady=(15,10))
@@ -38,6 +38,8 @@ class ParentWindow(Frame):
 
     def destDir(self):
         selectDestDir = tkinter.filedialog.askdirectory()
+        self.destination_dir.delete(0, END)
+        self.destination_dir.insert(0, selectDestDir)
 
 if __name__ == "__main__":
     root = tk.Tk()
