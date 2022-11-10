e199883025235642b02a5e627a21d8e8cb8d26a7
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-09 19:59:52 -0800

    defaulthtml button function add

--- web_page_generator.py
+++ web_page_generator.py
@@ -8,6 +8,13 @@ class ParentWindow(Frame):
         self.master.title("Web Page Generator")
         self.btn = Button(self.master, text="Default HTML Page", width = 30, height = 2)
         self.btn.grid(padx = (10,10), pady = (10,10))
+        def defaultHTML(self):
+            htmlText = "Stay tuned for our amazing summer sale!"
+            htmlFile = open("index.html", "w")
+            htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+            htmlFile.write(htmlContent)
+            htmlFile.close()
+            webbrowser.open_new_tab("index.html")
 
 
 
