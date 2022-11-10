1d629b3e6e3489ee02dcc6eb2a0407764696e7d5
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-09 19:53:41 -0800

    added basic webpage setup

---
+++ web_page_generator.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+
+class ParentWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master.title("Web Page Generator")
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    App = ParentWindow(root)
+    root.mainloop()
