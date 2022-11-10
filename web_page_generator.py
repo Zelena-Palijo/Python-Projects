dc48e0b17f51e44f28c080716107c574b9e4b5a9
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-09 19:55:54 -0800

    default html page button

--- web_page_generator.py
+++ web_page_generator.py
@@ -6,6 +6,8 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
+        self.btn = Button(self.master, text="Default HTML Page", width = 30, height = 2)
+        self.btn.grid(padx = (10,10), pady = (10,10))
 
 
 
