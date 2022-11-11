e14372996738047be4507f603a804f8608d157f4
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-11-11 12:34:02 -0800

    troubleshoot

--- web_page_generator.py
+++ web_page_generator.py
@@ -6,18 +6,17 @@ class ParentWindow(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        self.btn = Button(self.master, text="Default HTML Page", width = 30, height = 2)
+        self.btn = Button(self.master, text="Default HTML Page", width = 30, height = 2, command=self.defaultHTML)
         self.btn.grid(padx = (10,10), pady = (10,10))
-        def defaultHTML(self):
-            htmlText = "Stay tuned for our amazing summer sale!"
-            htmlFile = open("index.html", "w")
-            htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
-            htmlFile.write(htmlContent)
-            htmlFile.close()
-            webbrowser.open_new_tab("index.html")
+        
 
-
-
+    def defaultHTML(self):
+        htmlText = "Stay tuned for our amazing summer sale!"
+        htmlFile = open("index.html","w")
+        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
 
 
 
